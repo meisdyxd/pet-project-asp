f20fc4020fb2b236bbad550ab36f67895289f27c
meisdyxd <kararturkar@gmail.com>
2025-08-17 20:10:07 +0300

    feat: add gitignore, editorconfig, swagger

---
+++ DirectoryService/src/DirectoryService.API/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DirectoryService.Presentation.Controllers;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class TestController: ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> Test()
+    {
+        return await Task.FromResult(Ok("Hello world!"));
+    }
+}
