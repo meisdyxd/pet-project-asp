031667577c6acdd92ee10d629cf662715ff3a750
a-karlov <a-karlov@ASOFT21-PC001>
2025-08-18 15:08:57 +0300

    feat: add docker compose(pgsql), add db context

---
+++ DirectoryService/src/DirectoryService.Infrastructure/Database/Context/DirectoryServiceContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DirectoryService.Infrastructure.Database.Context;
+
+public class DirectoryServiceContext : DbContext
+{
+    public DirectoryServiceContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+    }
+}
---
+++ DirectoryService/src/DirectoryService.Presentation/Program.cs
@@ -0,0 +1,24 @@
+﻿using DirectoryService.Presentation.RegisterServices;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+
+services
+    .SwaggerConfigure()
+    .AddControllers();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{ 
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllers();
+app.Run();
