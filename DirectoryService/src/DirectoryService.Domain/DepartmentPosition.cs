bf56df61e9acc74fa508ccd894042e34b8c4be03
meisdy <kararturkar@gmail.com>
2025-08-19 21:27:58 +0300

    fix: add constructor to junction entity

--- DirectoryService/src/DirectoryService.Domain/DepartmentPosition.cs
+++ DirectoryService/src/DirectoryService.Domain/DepartmentPosition.cs
@@ -1,15 +1,31 @@
-﻿namespace DirectoryService.Domain;
+﻿using CSharpFunctionalExtensions;
+using DirectoryService.Contracts;
+
+namespace DirectoryService.Domain;
 
 public class DepartmentPosition
 {
-    protected DepartmentPosition()
+    protected DepartmentPosition() { }
+    
+    private DepartmentPosition(Guid positionId, Guid departmentId)
     {
-
+        PositionId = positionId;
+        DepartmentId = departmentId;
     }
 
-    public Guid DepartmentId { get; set; }
-    public Guid PositionId { get; set; }
+    public Guid DepartmentId { get; private set; }
+    public Guid PositionId { get; private set; }
 
-    public Department Department { get; set; }
-    public Position Position { get; set; }
+    public Department Department { get; private set; }
+    public Position Position { get; private set; }
+    
+    public static Result<DepartmentPosition, Error> Create(Guid positionId, Guid departmentId)
+    {
+        if (positionId == Guid.Empty)
+            return Errors.InvalidValue.Default(nameof(positionId));
+        if (departmentId == Guid.Empty)
+            return Errors.InvalidValue.Default(nameof(departmentId));
+        
+        return new DepartmentPosition(positionId, departmentId);
+    }
 }
