12d0525effdc7f14a8537382730ef754aa05fbe9
meisdy <kararturkar@gmail.com>
2025-09-15 19:12:26 +0300

    feat: add migration for add department

---
+++ DirectoryService/src/DirectoryService.Infrastructure/Database/Migrations/20250915134122_AddParentIdFIeldToEntity.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DirectoryService.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddParentIdFIeldToEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:PostgresExtension:uuid-ossp", ",,");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "id",
+                table: "positions",
+                type: "uuid",
+                nullable: false,
+                defaultValueSql: "uuid_generate_v4()",
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "id",
+                table: "locations",
+                type: "uuid",
+                nullable: false,
+                defaultValueSql: "uuid_generate_v4()",
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "id",
+                table: "departments",
+                type: "uuid",
+                nullable: false,
+                defaultValueSql: "uuid_generate_v4()",
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .OldAnnotation("Npgsql:PostgresExtension:uuid-ossp", ",,");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "id",
+                table: "positions",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldDefaultValueSql: "uuid_generate_v4()");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "id",
+                table: "locations",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldDefaultValueSql: "uuid_generate_v4()");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "id",
+                table: "departments",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldDefaultValueSql: "uuid_generate_v4()");
+        }
+    }
+}
